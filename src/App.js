2fd02306f4b1e4bddc6aef5d4a3e4f1b8f28b077
jungmin <43zhuzhu@gmail.com>
2025-06-04 17:07:01 +0900

    컴포넌트에 Props전달하기

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
-import { getImageUrl } from "./utils"
+import './App.css';
+import Avatar from "./Avatar.js";
 
-function Avatar({ person, size }) {
+function Card({ children }) {
   return (
-    <img
-    className="avator"
-    src={getImageUrl(person)}
-    alt={person.name}
-    width={size}
-    height={size}
-    />
+    <div className="card">
+      {children}
+    </div>
   )
 }
 
 export default function Profile() {
   return (
-    <div>
+    <Card>
       <Avatar
+        person={{ name: 'Katsuko Saruhashi', imageId: 'YfeOqp2' }}
         size={100}
-        person={{
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2'
-        }}
       />
-      <Avatar
-        size={80}
-        person={{
-          name: 'Aklilu Lemma',
-          imageId: 'OKS67lh'
-        }}
-      />
-      <Avatar
-        size={50}
-        person={{
-          name: 'Lin Lanying',
-          imageId: '1bX5QH6'
-        }}
-      />
-    </div>
+    </Card>
   )
 }
-
-// https://ko.react.dev/learn/passing-props-to-a-component
