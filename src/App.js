7be336da067d921e64e282bcd9416f81e5b1d61d
jungmin <43zhuzhu@gmail.com>
2025-06-05 18:07:30 +0900

    조건부 렌더링 예제

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-function Item({name, isPacked}) {
-  let itemContent = name;
-  if(isPacked) {
-    itemContent = name + ' ✅';
+function Drink({name}) {
+  let part, caffeine, age;
+  if(name === 'tea') {
+    part = 'leaf';
+    caffeine = '15-70 mg/cup';
+    age = '4,000+ years';
+  } else if(name === 'coffee') {
+    part = 'bean';
+    caffeine = '80-185 mg/cup';
+    age = '1,000+ years';
   }
   return (
-    <li className="item">
-      {itemContent}
-    </li>
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{part}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{caffeine}</dd>
+        <dt>Age</dt>
+        <dd>{age}</dd>
+      </dl>
+    </section>
   )
 }
 
-export default function PackingList() {
+export default function DrinkList() {
   return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item
-          isPacked={true}
-          name="Space suit"
-        />
-        <Item
-          isPacked={true}
-          name="Helmet with a golden leaf"
-        />
-        <Item
-          isPacked={false}
-          name="Photo of Tam"
-        />
-      </ul>
-    </section>
+    <div>
+      <Drink name="tea" />
+      <Drink name="coffee" />
+    </div>
   )
 }
