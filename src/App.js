677b84a170b9fc168f7f0224be0ca335a85c69d4
jungmin <43zhuzhu@gmail.com>
2025-06-11 18:17:15 +0900

    리스트 렌더링예제문제

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-function Drink({name}) {
-  let part, caffeine, age;
-  if(name === 'tea') {
-    part = 'leaf';
-    caffeine = '15-70 mg/cup';
-    age = '4,000+ years';
-  } else if(name === 'coffee') {
-    part = 'bean';
-    caffeine = '80-185 mg/cup';
-    age = '1,000+ years';
-  }
+import { recipes } from "./data";
+
+function Recipe({name, ingredients}) {
   return (
-    <section>
-      <h1>{name}</h1>
-      <dl>
-        <dt>Part of plant</dt>
-        <dd>{part}</dd>
-        <dt>Caffeine content</dt>
-        <dd>{caffeine}</dd>
-        <dt>Age</dt>
-        <dd>{age}</dd>
-      </dl>
-    </section>
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map(ingredient =>
+          <li key={ingredient}>{ingredient}</li>
+        )}
+      </ul>
+    </div>
   )
 }
 
-export default function DrinkList() {
+export default function RecipeList() {
   return (
     <div>
-      <Drink name="tea" />
-      <Drink name="coffee" />
+      <h1>Recipes</h1>
+      {recipes.map(recipe => 
+        <Recipe name={recipe.name} ingredients={recipe.ingredients} key={recipe.id} />
+      )}
+      {/* {recipes.map(item =>
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+          <ul>
+            {item.ingredients.map(i => 
+              <li key={i}>{i}</li>
+            )}
+          </ul>
+        </div>
+      )} */}
     </div>
   )
 }
