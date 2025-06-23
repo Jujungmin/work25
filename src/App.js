b06f4e61fc27746f32d598ba24add1fcd8c4d9fb
jungmin <43zhuzhu@gmail.com>
2025-06-23 15:48:00 +0900

    State:배열 예제문제

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-function Item({name, importance}) {
-  return (
-    <li>
-      {name} {importance > 0 && (<i>(IMPORTANCE: {importance})</i>)}
-    </li>
-  )
-}
+import { useState } from "react";
 
-export default function PackingList() {
+export default function Movie() {
+  const [input, setInput] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(input.trim() === '') {
+      alert('영화 제목을 입력해주세요.');
+      return;
+    }
+    setMovies([...movies, input]);
+    setInput('');
+    console.log(movies)
+  }
+  function handleDelete(index) {
+    return () => {
+      setMovies(
+        movies.filter((_, i) => {
+          console.log(_, i);
+          return i !== index;
+        })
+      )
+    }
+  }
   return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item
-          importance={9}
-          name="Space suit"
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="영화를 입력해주세요."
+          value={input}
+          onChange={e => setInput(e.target.value)}
         />
-        <Item
-          importance={0}
-          name="Helmet with a golden leaf"
-        />
-        <Item
-          importance={6}
-          name="Photo of Tam"
-        />
-      </ul>
-    </section>
+        <button type="submit">등록</button>
+      </form>
+      {movies.length > 0 && (
+        <ul style={{border:'1px solid #222'}}>
+          {movies.map((movie, index) => (
+            <li key={index} style={{padding:'10px'}}>
+              {movie}
+              <button onClick={handleDelete(index)}>삭제</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   )
 }
